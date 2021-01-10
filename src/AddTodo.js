5cc2c7ac843340d01268c8c9579d4c5f1e399437
Nadeeja99 <nadeejaperera123@gmail.com>
2021-01-10 12:44:36 +0530

    first commit

---
+++ src/AddTodo.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'; 
+
+class AddTodo extends Component {
+    render() {
+        return (
+            <div>
+                <form onSubmit={}>
+                    <label>Add new todo:</label>
+                    <input type="text" onChange={} />
+                </form>
+            </div>
+        )
+    }
+}
