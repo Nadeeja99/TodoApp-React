703c4d85a3b99b565e0ef47cd733d6781a456d7b
Nadeeja99 <nadeejaperera123@gmail.com>
2021-01-10 13:02:06 +0530

    add todo

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,14 +1,36 @@
 import React, {Component} from 'react'; 
 
 class AddTodo extends Component {
+    state = {
+        content: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            content: e.target.value
+          });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+        // call function to add a todo
+        this.props.addTodo(this.state);
+        this.setState({
+          content: ''
+        })
+      }
+
     render() {
         return (
             <div>
-                <form onSubmit={}>
+                <form onSubmit={this.handleSubmit}>
                     <label>Add new todo:</label>
-                    <input type="text" onChange={} />
+                    <input type="text" onChange={this.handleChange} value={this.state.content} />
                 </form>
             </div>
         )
     }
 }
+
+export default AddTodo
